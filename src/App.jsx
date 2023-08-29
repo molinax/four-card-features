93401c7257cadcf9b72c415b64531b35e138b364
molina <arielmolina_55@hotmail.com>
2023-08-28 22:44:21 -0300

    new grid-column in md breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ export const App = () => {
           </p>
         </header>
 
-        <section className="grid grid-cols-1 grid-rows-2 lg:grid-cols-3 lg:grid-rows-4 gap-8">
+        <section className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-4 gap-8">
           {featureCards.map((featureCard, index) => {
             return <FeatureCard
               key={index}
